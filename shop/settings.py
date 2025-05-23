1367274a332980de7501b26515362d6a4d16c59e
Vovikses <vvikhmanov@gmail.com>
2025-05-23 19:11:43 +0100

    переробив базу даних

--- shop/settings.py
+++ shop/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'main',
     'cart',
     'users',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -83,13 +84,8 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'NAME': os.getenv('POSTGRES_DB', 'db01'),
-        'ATMOIC_REQUEST': True,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
